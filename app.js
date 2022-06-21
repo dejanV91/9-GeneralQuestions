56d38ddc8b4abf31dd26926007e07c009866e140
dejanV91 <dejanvukoicic91@gmail.com>
2022-06-21 21:31:35 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-const addButton= document.querySelectorAll(".adder");
-
-
-addButton.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-       const parentElem=e.currentTarget.parentNode.parentNode;
-       const hiddenElem=parentElem.children.item(1)
-       hiddenElem.classList.toggle("showHidden");
-        
-    })
-});
 
 
