f921458258c3743c6380f47e1fb68f48852e3491
dejanV91 <dejanvukoicic91@gmail.com>
2022-06-22 14:44:37 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+// daje svako pitanje
+const questionNode=document.querySelectorAll(".btn");
+// pristupam + i - svakom NodeList
+questionNode.forEach(function(item){
+    
+    const allButtons=item.querySelector(".buttons");
+    
+    
+    allButtons.addEventListener('click', function(){
+        questionNode.forEach(function(elem){
+            if (elem !== item) {
+                elem.classList.remove("show-text");
+            }
+        item.classList.toggle("show-text");
+        
+    });
+});
 
+});
 
